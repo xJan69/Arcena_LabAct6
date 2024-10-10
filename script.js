d195f0db4f31099993782516c36214a5cffaa9c6
Jan <janarcena13@gmail.com>
2024-10-10 10:45:42 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,37 +2,33 @@ $(() => {
     let details = [];
     fetch("curriculum.json")
     .then((rawData)=> rawData.json())
-    .then(({semesters, courses})=>{
+    .then(({Semesters, Courses})=>{
     console.log(details);
 
-    semesters.forEach(sem =>{
-        $("#table-with-data").append(
-            `<th colspan="7">${sem}</th>
-            <tr>
-                <th>Course</th>
-                <th>Description</th>
-                <th>Unit</th>
-                <th>Grade</th>
-                <th>Remarks</th>
-                <th>Course</th>
-                <th>Term</th>
-            </tr>`
-        )
-        $("#table-with-data").append(
-            `<tr class="${sem-head}">
-                <td>${courses["course"]}</td>
-                <td>${courses["des"]}</td>
-                <td>${courses["unit"]}</td>
-                <td>${courses["grade"]}</td>
-                <td>${courses["remarks"]}</td>
-                <td>${courses["course"]}</td>
-                <td>${courses["term"]}</td>`
-        );
-    })
-})
-
-
-
-
-
+        Semesters.forEach(sem =>{
+            $("#table-with-data").append(
+                `<th colspan="7">${sem.description}</th>
+                <tr>
+                    <th>Course</th>
+                    <th>Description</th>
+                    <th>Unit</th>
+                    <th>Grade</th>
+                    <th>Remarks</th>
+                    <th>Course</th>
+                    <th>Term</th>
+                </tr>`
+            );
+            $("#table-with-data").append(
+                `<tr class="${sem}">
+                    <td>${Courses["course"]}</td>
+                    <td>${Courses["description"]}</td>
+                    <td>${Courses["unit"]}</td>
+                    <td>${Courses["grade"]}</td>
+                    <td>${Courses["remarks"]}</td>
+                    <td>${Courses["term"]}</td>
+                </tr>`
+            );
+            console.log(sem);
+        });
+    });
 });
